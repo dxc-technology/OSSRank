fb8fc8b61f01bc416d0cb05a7919f258ee040581
theroys <aroy25@csc.com>
2015-03-09 09:33:51 +0100

    update

--- data/stackexchange_data_load.py
+++ data/stackexchange_data_load.py
@@ -29,21 +29,36 @@ def get_projects_mongolab():
      res = requests.get(query_url,headers=headers)
      projects = res.json()
      count = 0
-     #print projects
+     
+     #iterate
      for p in projects:
         project = p
         
-        #escape wrong data
+        #escape wrong data in mongo lab if there is no name no search possible
         if(not project.has_key('name')):
             continue
+        
         print 'updating   ' + project['name']
         
+        #find document id for update
         oid=project ['_id']
         document_id = oid['$oid']
-        ##get category
-        stack_tag_total = search_for_tag(project['name'])
-        stack_intitle_total = search_in_title(project['name'])
-        ##update json with stackexchange data
+        #get stackoverflow result
+        
+        project_name=project['name']
+        project_name_alnum=get_alaphanumeric_string(project_name)
+        
+        #does project name contains special char search with or without
+        #modify it to search similar name -- todo
+        
+        if(project_name is project_name_alnum ):
+            stack_tag_total = search_for_tag(project['name'])
+            stack_intitle_total = search_in_title(project['name'])
+        else:
+            stack_tag_total = search_for_tag(project['name'])+search_for_tag(project_name_alnum)
+            stack_intitle_total = search_in_title(project['name'])+ search_in_title(project_name_alnum)
+            
+        #update json with stackexchange data
         today=time.strftime("%d.%m.%Y")
         stack_exchange_todays_dict={'added_at':today,'questions':stack_intitle_total, 'tagged_dicsussion':stack_tag_total}
         
@@ -61,9 +76,14 @@ def get_projects_mongolab():
             break
         
 
-
-
-
+#removes special character to do a similarity search
+def get_alaphanumeric_string(project_name):
+    if(not project_name.isalnum()):
+        new_name= ''.join(e for e in project_name if e.isalnum())
+        print new_name
+        return new_name
+    else:
+        return project_name
 
 def search_for_tag(project_name):
     
@@ -93,3 +113,4 @@ def update_projects(project, document_id):
 if __name__ == '__main__':
     #search_for('bootstrap')
     get_projects_mongolab()
+    
