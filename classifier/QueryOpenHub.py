27cfee14bb2ac7a7a80ae4ddd78ec0171ee865f5
theroys <aroy25@csc.com>
2015-04-22 12:25:59 +0200

    updates & fixes

--- classifier/QueryOpenHub.py
+++ classifier/QueryOpenHub.py
@@ -20,6 +20,7 @@ OPENHUB_SEARCH_URL = 'https://www.openhub.net/projects.xml'
 #OPENHUB_API_KEY = '7u9sgYGrr6nv2snYk8o7w'
 OPENHUB_API_KEY = '40fcNIhGvhr3kPfRpH0IkQ'
 
+
 ######################
 #method returns the openhubquery
 ######################
@@ -31,64 +32,61 @@ def getOpenHubQuery(gitProjectName):
 
 
 def queryOpenhubDetails(gitProjectName):
-    ##uncomment below for debug only
-    #print getOpenHubQuery('bootstrap')
-    
-    f=urllib.urlopen(getOpenHubQuery(gitProjectName))
-    
-    
-    tree = ElementTree.parse(f)
-    elem = tree.getroot()
-    
-        
-    
-    #check for error first
-    error=elem.find('error')
     
     projectDataDictionary= {}
     
-    if error != None:
-        print 'openhub api returned an error while searching for project '+ gitProjectName + ElementTree.tostring(error)
-        return projectDataDictionary 
+    try:
     
-    result_num=elem.find('items_available')
-    print 'search for '+  gitProjectName +' returned '  + result_num.text + 'results'
-    
-    #the strategy the query is always against name and desc, so we make sure name contains what we search
-    #if found collect the tags and then break the loop
-    #1000 req allowed per day
-    
-    ##following code is  not required we will iterate through all results
-    #for node in tree.find('result/project'):
-    #    if node.tag == 'name':
-    #        print node.text
-    #     if node.tag == 'homepage_url':
-    #         print node.text
-    
-    prj_found=False
-    for Element in tree.findall('.//project'):
+        f=urllib.urlopen(getOpenHubQuery(gitProjectName))
         
-        project_name= Element.find('.//name').text
-        download_url= Element.find('.//download_url').text
-        #print download_url
-        if(gitProjectName.lower() in project_name.lower() ):
-            #check if the download  url contains both github.com and ends with gitprojectname
-            if( (download_url is not None) and('github.com' in download_url) and (gitProjectName.lower() in download_url) ):
-                project_desc= Element.find('.//description').text
-                print "matching projects " + project_name 
-                projectDataDictionary['desc']= project_desc
- 
-                tags=''
-                for Element in Element.findall('.//tag'):
-                    tags=tags +'  '+ Element.text
-                #print 'project specific tags are' + tags
-                
-                if(tags != ''):
-                    projectDataDictionary['tag']= tags
-                prj_found=True
-            #print projectDataDictionary.get('tag')
-        if(prj_found): break
-         
+        
+        tree = ElementTree.parse(f)
+        elem = tree.getroot()
+        
+            
+        
+        #check for error first
+        error=elem.find('error')
+        
+        
+        if error != None:
+            print 'openhub api returned an error while searching for project '+ gitProjectName + ElementTree.tostring(error)
+            return projectDataDictionary 
+        
+        result_num=elem.find('items_available')
+        print 'search for '+  gitProjectName +' returned '  + result_num.text + 'results'
+        
+        #the strategy the query is always against name and desc, so we make sure name contains what we search
+        #if found collect the tags and then break the loop
+        #1000 req allowed per day
+        
+        
+        prj_found=False
+        for Element in tree.findall('.//project'):
+            
+            project_name= Element.find('.//name').text
+            download_url= Element.find('.//download_url').text
+            #print download_url
+            if(gitProjectName.lower() in project_name.lower() ):
+                #check if the download  url contains both github.com and ends with gitprojectname
+                if( (download_url is not None) and('github.com' in download_url) and (gitProjectName.lower() in download_url) ):
+                    project_desc= Element.find('.//description').text
+                    print "matching projects " + project_name 
+                    projectDataDictionary['desc']= project_desc
+     
+                    tags=''
+                    for Element in Element.findall('.//tag'):
+                        tags=tags +'  '+ Element.text
+                    #print 'project specific tags are' + tags
+                    
+                    if(tags != ''):
+                        projectDataDictionary['tag']= tags
+                    prj_found=True
+                #print projectDataDictionary.get('tag')
+            if(prj_found): break
+    
+    except Error as e:
+        print "Error occured fetching data from openhub", e
     return projectDataDictionary
     
     ##test code -- to be deleted
