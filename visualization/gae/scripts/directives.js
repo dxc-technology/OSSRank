1413017d47d6bc3660f2ea3456a38c3703af6b60
fsiddiqi <fsiddiqi@gmail.com>
2016-02-09 18:25:51 -0500

    Site and home page CSS cleanup.  Changed Bootstrap theme.  Added progress indicator on Search: #45.  Tightened ip query

--- visualization/gae/scripts/directives.js
+++ visualization/gae/scripts/directives.js
@@ -113,4 +113,26 @@ angular.module('ossrank.directives',[]).directive('autoComplete',['$http',functi
             });
         }
     }
-}]);
+}])
+    .directive('loading',   ['$http' ,function ($http)
+    {
+        return {
+            restrict: 'A',
+            link: function (scope, elm, attrs)
+            {
+                scope.isLoading = function () {
+                    return $http.pendingRequests.length > 0;
+                };
+
+                scope.$watch(scope.isLoading, function (v)
+                {
+                    if(v){
+                        $(elm).show();
+                    }else{
+                        $(elm).hide();
+                    }
+                });
+            }
+        };
+
+    }]);
