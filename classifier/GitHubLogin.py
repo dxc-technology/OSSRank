e736bce822c4661b83f6c75300b94848e18d4257
Adam Harvey <adam.harvey@dxc.com>
2018-01-29 22:42:28 -0500

    Single typo and whitespace update

--- classifier/GitHubLogin.py
+++ classifier/GitHubLogin.py
@@ -11,32 +11,28 @@ from urlparse import urljoin
 #Define our function to return Github Oauth token
 #Once the token is generated it can be used by collection API
 def getGithubOauthtoken():
-    
+
     username = raw_input('Your Github username : ')
     password = getpass.getpass('your Github password :')
     #the note will be asociate the specific  token generated in your account
     note=raw_input('Personal Note(Optional):')
-    
+
     url = urljoin(GITHUB_API , 'authorizations')
     payload = {}
     if note:
         payload['note']=note
     res= requests.post(
-                      url, 
-                      auth=(username, password), 
-                      data=json.dumps(payload), 
+                      url,
+                      auth=(username, password),
+                      data=json.dumps(payload),
                       )
-    #print statemnets are for debug only
+    #print statements are for debug only
     print res.status_code
     print res.headers['content-type']
     print res.text
-    
+
     #parsing json response from git hub
     j=json.loads(res.text)
     token=j['token']
     print 'Github Token;%s'%token
     return token
-    
-    
-
-
